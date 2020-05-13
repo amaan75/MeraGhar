34ba96ae35e0b6e49fba0e1e45b3ef5881c34e28
Mohit Kumar <mkumar@tekion.com>
2020-05-13 23:31:54 +0530

    Creates initial server scaffolding

---
+++ server/my-society/src/test/kotlin/org/basementstudios/mysociety/MySocietyApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.basementstudios.mysociety
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MySocietyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
